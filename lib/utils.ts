ee02151641891e122da63d51a3418d70367f0d5c
adriendelagneau <delagneauadrien@yahoo.fr>
2023-12-18 12:28:00 +0100

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from 'clsx'
+
+import { twMerge } from 'tailwind-merge'
+
+
+//import { UrlQueryParams, RemoveUrlQueryParams } from '@/types'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export const handleError = (error: unknown) => {
+  console.error(error)
+  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
+}
