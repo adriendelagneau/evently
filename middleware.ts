79d624ddc0959e14f4014726eab98cf0ef8495a5
adriendelagneau <delagneauadrien@yahoo.fr>
2023-12-18 20:41:22 +0100

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-   // '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+  publicRoutes: ["/api/webhooks(.*)"]
 });
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
